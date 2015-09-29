e5dc1cfb15440d7a022b9a64e0d98014d0196acc
duinn <dunnep6@tcd.ie>
2015-09-29 22:35:56 +0100

    basic scraping of player array

--- scriplets/json_reader.rb
+++ scriplets/json_reader.rb
@@ -1,3 +1,13 @@
 require 'nokogiri'
 
+require 'json'
 
+file = File.open("sample html/fantasyfootball.html")
+page = Nokogiri::HTML(file)
+
+data_hash = JSON.parse(page.css('div#ismJson').css('script').text)
+
+p data_hash['elInfo']
+# elInfo = array of all players
+# eiwteams = json of teams, their names and short names
+# teamInfo = next game week fixtures
